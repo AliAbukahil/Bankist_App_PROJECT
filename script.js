b8abf413184989eaf12522f450f55fd604989d62
ali.abukahil <ali.abukahil@gmail.com>
2021-11-27 00:13:07 +0100

    The Map Method

--- script.js
+++ script.js
@@ -248,3 +248,32 @@ GOOD LUCK 😀
 // };
 // // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 // checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+// The Map method
+const euroToUsd = 1.1;
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const movementsUSD = movements.map(function (mov, i, arr) {
+//   // console.log(mov, i, arr);
+//   return mov * euroToUsd;
+// });
+
+//the map method with Arrow function
+const movementsUSD = movements.map((mov) => mov * euroToUsd);
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * euroToUsd);
+
+console.log(movementsUSDfor);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? "deposited" : "withdrew"} ${Math.abs(
+      mov
+    )}`
+);
+
+console.log(movementsDescriptions);
